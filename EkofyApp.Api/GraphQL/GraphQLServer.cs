f3b51734208e873954dcfa077ad3f29c3017c261
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-07-20 17:04:37 +0700

    Update AWS SDK packages and add Redis support
    Upgraded AWSSDK.Core to 4.0.0.16 and AWSSDK.S3 to 4.0.5.1. Added HotChocolate.PersistedQueries.Redis package for persisted query support. Updated GraphQLServer to include commented code for Redis query storage integration.

--- EkofyApp.Api/GraphQL/GraphQLServer.cs
+++ EkofyApp.Api/GraphQL/GraphQLServer.cs
@@ -2,6 +2,7 @@
 using EkofyApp.Api.GraphQL.Mutation;
 using EkofyApp.Api.GraphQL.Query;
 using EkofyApp.Api.GraphQL.Scalars;
+using StackExchange.Redis;
 
 namespace EkofyApp.Api.GraphQL;
 public static class GraphQLServer
@@ -11,8 +12,14 @@ public static class GraphQLServer
         services.AddGraphQLServer().AddErrorFilter<GraphQLExceptionFilter>()
             .AddAuthorization()
             // Nếu expose field có data type UInt32 thì cần phải bind nó vào GraphQL
-            .AddType<UploadType>().AddType(new UInt32Type())
-            .BindRuntimeType<uint, UInt32Type>()
+            .AddType<UploadType>()
+            .AddType(new UInt32Type()).BindRuntimeType<uint, UInt32Type>()
+            //.UsePersistedQueryPipeline()
+            //.AddRedisQueryStorage(sp =>
+            //{
+            //    string config = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING_N0_SSL")!;
+            //    return ConnectionMultiplexer.Connect(config);
+            //})
             .AddMaxExecutionDepthRule(5).AddMaxAllowedFieldCycleDepthRule(50)
             .AddProjections()
             .AddMongoDbFiltering().AddMongoDbSorting().AddMongoDbPagingProviders()
