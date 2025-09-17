39c7fb6f98ced848aa1081c7ffd97d7d1505fdb8
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-09-17 16:09:33 +0700

    Add Redis hash operations and update service logic
    Introduced new methods to IRedisCacheService and RedisCacheService for hash operations: GetHashAsync, HashIncrementAsync, and HashFieldExpireAsync. Updated TrackService with a stub for UpdateStreamCount. Improved environment variable handling in DependencyInjection and AmazonCloudFrontService. Refactored BackgoundService for cleaner structure and commented out a Hangfire job enqueue in HangfireJobConfiguration.

--- EkofyApp.Infrastructure/Services/Jobs/BackgoundService.cs
+++ EkofyApp.Infrastructure/Services/Jobs/BackgoundService.cs
@@ -5,54 +5,52 @@ using Hangfire.Server;
 using System.Net;
 using System.Net.Mail;
 
-namespace EkofyApp.Infrastructure.Services.Jobs
+namespace EkofyApp.Infrastructure.Services.Jobs;
+public class BackgoundService : IBackgoundService
 {
-    public class BackgoundService : IBackgoundService
+    //[Queue("default")]
+    [JobDisplayName("Test display Background Job")]
+    public void DisplayLogTest(PerformContext context)
     {
-        //[Queue("default")]
-        [JobDisplayName("Test display Background Job")]
-        public void DisplayLogTest(PerformContext context)
+        Console.WriteLine("Test Background Job is running...");
+        Console.WriteLine($"Test Background Job has completed: {DateTime.Now}");
+    }
+
+
+    [Queue("default")]
+    [JobDisplayName("Send Email")]
+    public void SendEmail(string toEmail)
+    {
+        try
         {
-            Console.WriteLine("Test Background Job is running...");
-            Console.WriteLine($"Test Background Job has completed: {DateTime.Now}");
+            SmtpClient smtpClient = new SmtpClient(Environment.GetEnvironmentVariable("EMAIL_SMTP_HOST"))
+            {
+                Port = Environment.GetEnvironmentVariable("EMAIL_SMTP_PORT") != null ? int.Parse(Environment.GetEnvironmentVariable("EMAIL_SMTP_PORT")!) : 587,
+
+                Credentials = new NetworkCredential(Environment.GetEnvironmentVariable("EMAIL_SMTP_USERNAME"), Environment.GetEnvironmentVariable("EMAIL_SMTP_PASSWORD")),
+
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(Environment.GetEnvironmentVariable("EMAIL_SMTP_USERNAME")!),
+                Subject = "Test gửi email từ EkofyApp",
+                Body = "<h1>Chào bạn, đây là email được gửi từ EkofyApp</h1><p>Đây là nội dung của email.</p>",
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(toEmail);
+
+            mailMessage.Headers.Add("X-Priority", "1");
+            mailMessage.Headers.Add("X-MSMail-Priority", "High");
+            mailMessage.Headers.Add("Importance", "High");
+
+            smtpClient.Send(mailMessage);
+            Console.WriteLine($"Email sent to {toEmail}");
         }
-
-
-        [Queue("default")]
-        [JobDisplayName("Send Email")]
-        public void SendEmail(string toEmail)
+        catch (Exception ex)
         {
-            try
-            {
-                SmtpClient smtpClient = new SmtpClient(Environment.GetEnvironmentVariable("EMAIL_SMTP_HOST"))
-                {
-                    Port = Environment.GetEnvironmentVariable("EMAIL_SMTP_PORT") != null ? int.Parse(Environment.GetEnvironmentVariable("EMAIL_SMTP_PORT")!) : 587,
-
-                    Credentials = new NetworkCredential(Environment.GetEnvironmentVariable("EMAIL_SMTP_USERNAME"), Environment.GetEnvironmentVariable("EMAIL_SMTP_PASSWORD")),
-
-                    EnableSsl = true,
-                };
-
-                var mailMessage = new MailMessage
-                {
-                    From = new MailAddress(Environment.GetEnvironmentVariable("EMAIL_SMTP_USERNAME")!),
-                    Subject = "Test gửi email từ EkofyApp",
-                    Body = "<h1>Chào bạn, đây là email được gửi từ EkofyApp</h1><p>Đây là nội dung của email.</p>",
-                    IsBodyHtml = true,
-                };
-                mailMessage.To.Add(toEmail);
-
-                mailMessage.Headers.Add("X-Priority", "1");
-                mailMessage.Headers.Add("X-MSMail-Priority", "High");
-                mailMessage.Headers.Add("Importance", "High");
-
-                smtpClient.Send(mailMessage);
-                Console.WriteLine($"Email sent to {toEmail}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to send email to {toEmail}. Error: {ex.Message}");
-            }
+            Console.WriteLine($"Failed to send email to {toEmail}. Error: {ex.Message}");
         }
     }
 }
