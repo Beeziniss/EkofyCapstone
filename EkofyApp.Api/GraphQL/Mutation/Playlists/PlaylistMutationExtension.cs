06f0d71a79549c46cbbd9ef54b61332c872863ed
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-10-13 22:20:29 +0700

    Add GraphQL mutations for playlist management
    Introduces PlaylistMutation and PlaylistMutationExtension to support playlist creation, modification, and deletion via GraphQL. Authorization is enforced for all playlist mutation operations using defined listener and artist roles.

---
+++ EkofyApp.Api/GraphQL/Mutation/Playlists/PlaylistMutationExtension.cs
@@ -0,0 +1,24 @@
+﻿using EkofyApp.Domain.Utils;
+
+namespace EkofyApp.Api.GraphQL.Mutation.Playlists;
+
+public sealed class PlaylistMutationExtension : ObjectTypeExtension<PlaylistMutation>
+{
+    protected override void Configure(IObjectTypeDescriptor<PlaylistMutation> descriptor)
+    {
+        descriptor.Field(x => x.CreatePlaylistAsync(default!))
+            .Authorize(HelperRoleBase.ListenerArtistRolesArray);
+
+        descriptor.Field(x => x.AddToFavoriteAsync(default!))
+            .Authorize(HelperRoleBase.ListenerArtistRolesArray);
+
+        descriptor.Field(x => x.AddToPlaylistAsync(default!))
+            .Authorize(HelperRoleBase.ListenerArtistRolesArray);
+
+        descriptor.Field(x => x.RemoveFromPlaylistAsync(default!))
+            .Authorize(HelperRoleBase.ListenerArtistRolesArray);
+
+        descriptor.Field(x => x.DeletePlaylistAsync(default!))
+            .Authorize(HelperRoleBase.ListenerArtistRolesArray);
+    }
+}
