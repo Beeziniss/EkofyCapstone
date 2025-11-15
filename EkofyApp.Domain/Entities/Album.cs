af802e31f59bf332e14447690d2b5b3a7dba34c5
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-11-16 02:10:46 +0700

    Remove IEntityCustom interface from Album and Playlist
    The Album and Playlist entity classes no longer implement the IEntityCustom interface. This change may be part of a refactor to simplify entity inheritance or remove unused interface functionality.

--- EkofyApp.Domain/Entities/Album.cs
+++ EkofyApp.Domain/Entities/Album.cs
@@ -5,7 +5,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace EkofyApp.Domain.Entities;
-public sealed class Album : TimeStamped, IEntityCustom
+public sealed class Album : TimeStamped
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
