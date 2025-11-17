368e8e4d12fc393d8d45b0b2f28358049a5e3abe
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-11-17 20:30:23 +0700

    Remove user restriction check from CreateRequestAsync
    Eliminates the user restriction validation and related dependency from the RequestMutation class. The CreateRequestAsync method now directly calls the request service without checking user restrictions.

--- EkofyApp.Api/GraphQL/Mutation/RequestHubs/RequestMutation.cs
+++ EkofyApp.Api/GraphQL/Mutation/RequestHubs/RequestMutation.cs
@@ -7,19 +7,12 @@ namespace EkofyApp.Api.GraphQL.Mutation.RequestHubs
 {
     [ExtendObjectType(typeof(MutationInitialization))]
     [MutationType]
-    public class RequestMutation(IRequestService requestHubService, IUserService userService)
+    public class RequestMutation(IRequestService requestHubService)
     {
         private readonly IRequestService _requestHubService = requestHubService;
-        private readonly IUserService _userService = userService;
 
         public async Task<bool> CreateRequestAsync(RequestCreatingRequest request)
         {
-            bool hasAnyRestriction = await _userService.CheckMultipleRestrictionsAsync(RestrictionAction.CreatePublicRequest);
-            if (hasAnyRestriction)
-            {
-                throw new UnauthorizedAccessException("You are restricted from creating public request.");
-            }
-
             return await _requestHubService.CreateRequestAsync(request);
         }
 
