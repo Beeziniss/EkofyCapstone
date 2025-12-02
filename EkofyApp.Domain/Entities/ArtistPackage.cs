d21ddfa148b50b99cae47bc663e096eda465258a
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-12-02 22:20:16 +0700

    Refactor ArtistPackage entity and update CustomPackageInfo
    Changed namespace declaration to file-scoped, removed IEntityCustom interface, and made CustomPackageInfo property nullable. These changes improve code clarity and align with modern C# conventions.

--- EkofyApp.Domain/Entities/ArtistPackage.cs
+++ EkofyApp.Domain/Entities/ArtistPackage.cs
@@ -3,28 +3,26 @@ using EkofyApp.Domain.EmbeddedDocuments;
 using EkofyApp.Domain.Enums;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace EkofyApp.Domain.Entities
+namespace EkofyApp.Domain.Entities;
+
+public class ArtistPackage : TimeStamped
 {
-    public class ArtistPackage : TimeStamped, IEntityCustom
-    {
-        [BsonId]
-        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string Id { get; set; } = null!;
-        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string ArtistId { get; set; } = null!;
-        public string PackageName { get; set; } = null!;
-        public decimal Amount { get; set; }
-        public CurrencyType Currency { get; set; } = CurrencyType.vnd;
-        public int EstimateDeliveryDays { get; set; }
-        public string? Description { get; set; }
-        public int MaxRevision { get; set; }
-        public List<Metadata> ServiceDetails { get; set; } = [];
-        public ArtistPackageStatus Status { get; set; }
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string ArtistId { get; set; } = null!;
+    public string PackageName { get; set; } = null!;
+    public decimal Amount { get; set; }
+    public CurrencyType Currency { get; set; } = CurrencyType.vnd;
+    public int EstimateDeliveryDays { get; set; }
+    public string? Description { get; set; }
+    public int MaxRevision { get; set; }
+    public List<Metadata> ServiceDetails { get; set; } = [];
+    public ArtistPackageStatus Status { get; set; }
 
-        // vì package được đánh theo version nên sẽ ko có xóa hay chỉnh sửa để tránh mấy khóa liên quan, cũng không cần biết thông tin ngày tạo
-        public bool IsDelete { get; set; } = false; // Indicates if it is visible to users
-        public bool IsCustom { get; set; } = false; // Indicates if it is a custom package
-        public CustomArtistPackageInfo CustomPackageInfo { get; set; } = null!;
-    }
-
+    // vì package được đánh theo version nên sẽ ko có xóa hay chỉnh sửa để tránh mấy khóa liên quan, cũng không cần biết thông tin ngày tạo
+    public bool IsDelete { get; set; } = false; // Indicates if it is visible to users
+    public bool IsCustom { get; set; } = false; // Indicates if it is a custom package
+    public CustomArtistPackageInfo? CustomPackageInfo { get; set; }
 }
