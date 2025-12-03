c46c2d0663e2416ae402622f87f51a86864e8d97
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-12-04 01:23:29 +0700

    Add IsPublic field to track update and adjust release logic
    Introduces the IsPublic property to UpdateTrackRequest and its validator, allowing tracks to be toggled between public and private. TrackService now updates release info based on IsPublic, setting release date and status accordingly. Also, URL validation for images and legal documents is commented out in relevant validators.

--- EkofyApp.Application/Models/Tracks/UpdateTrackRequest.cs
+++ EkofyApp.Application/Models/Tracks/UpdateTrackRequest.cs
@@ -6,4 +6,5 @@ public sealed record class UpdateTrackRequest
     public string? Description { get; init; }
     public List<string>? CategoryIds { get; init; }
     public List<string>? Tags { get; init; }
+    public bool? IsPublic { get; init; }
 }
