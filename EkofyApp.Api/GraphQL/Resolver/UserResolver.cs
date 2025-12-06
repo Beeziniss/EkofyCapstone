036da4d70848a7b28a110d66bb522e56d1db7c6c
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-12-06 22:06:24 +0700

    Add listener and artist queries to UserResolver
    Introduced GraphQL queries in UserResolver to fetch listeners and artists associated with a user, supporting paging, projection, filtering, and sorting. Also removed IEntityCustom interface from Subscription entity for simplification.

--- EkofyApp.Api/GraphQL/Resolver/UserResolver.cs
+++ EkofyApp.Api/GraphQL/Resolver/UserResolver.cs
@@ -1,5 +1,8 @@
-﻿using EkofyApp.Application.ServiceInterfaces.Users;
+﻿using EkofyApp.Application.ServiceInterfaces;
+using EkofyApp.Application.ServiceInterfaces.Users;
 using EkofyApp.Domain.Entities;
+using HotChocolate.Data;
+using MongoDB.Driver;
 
 namespace EkofyApp.Api.GraphQL.Resolver;
 
@@ -9,5 +12,23 @@ public sealed class UserResolver
     public async Task<bool> CheckUserFollowingAsync([Parent] User user, [Service] IUserService userService)
     {
         return await userService.CheckUserFollowingAsync(user.Id);
+    }
+
+    [UseOffsetPaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting<Listener>]
+    public IQueryable<Listener> GetListeners([Parent] User user, [Service] IUnitOfWork unitOfWork)
+    {
+        return unitOfWork.GetCollection<Listener>().AsQueryable().Where(listener => listener.UserId == user.Id);
+    }
+
+    [UseOffsetPaging(IncludeTotalCount = true)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting<Artist>]
+    public IQueryable<Artist> GetArtists([Parent] User user, [Service] IUnitOfWork unitOfWork)
+    {
+        return unitOfWork.GetCollection<Artist>().AsQueryable().Where(artist => artist.UserId == user.Id);
     }
 }
