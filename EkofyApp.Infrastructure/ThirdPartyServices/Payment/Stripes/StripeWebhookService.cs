2cf1dbee7181a4f1c2ae3c431e34bd1c9dce7971
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-12-11 00:34:50 +0700

    Update package order dispute and refund logic
    Changed status transitions in PackageOrderService to use 'Disputed' instead of 'Cancelled' for certain refund scenarios. Adjusted refund eligibility to only allow for 'Disputed' status. Updated StripeService to check for 'Completed' or 'Disputed' status instead of relying on 'CompletedAt'. Removed mobile test code from StripeWebhookService.

--- EkofyApp.Infrastructure/ThirdPartyServices/Payment/Stripes/StripeWebhookService.cs
+++ EkofyApp.Infrastructure/ThirdPartyServices/Payment/Stripes/StripeWebhookService.cs
@@ -797,12 +797,6 @@ public sealed class StripeWebhookService(IUnitOfWork unitOfWork, ILogger<StripeS
                 {
                     CheckoutOption.Session checkoutSession = stripeEvent.Data.Object as CheckoutOption.Session ?? throw new ArgumentNullCustomException("Checkout session is NULL");
 
-                    // For testing Mobile
-                    if (Convert.ToBoolean(checkoutSession.Metadata["is_mobile"]))
-                    {
-                        return;
-                    }
-
                     // Cập nhật PaymentTransaction
                     UpdateDefinition<PaymentTransaction> update = Builders<PaymentTransaction>.Update
                         .Set(t => t.PaymentStatus, PaymentTransactionStatus.Unpaid)
